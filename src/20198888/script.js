4a5971dc77f577eecb1126ada708842c13daff86
mohamedgonl <long.nq.hust@gmail.com>
2023-06-27 12:15:14 +0700

    send mail ok

--- src/20198888/script.js
+++ src/20198888/script.js
@@ -20,13 +20,6 @@ const SCOPES =
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
-console.log(document.getElementById("authorize_button"));
-document.getElementById("authorize_button").style.visibility = "hidden";
-document.getElementById("signout_button").style.visibility = "hidden";
-document.getElementById("data-table").style.visibility = "hidden";
-document.getElementById("send_mail_button").style.visibility = "hidden";
-document.getElementById("popup-form").style.display = "none";
-document.getElementById("popup-form-2").style.display = "none";
 
 /**
  * Callback after api.js is loaded.
@@ -73,15 +66,16 @@ function maybeEnableButtons() {
 /**
  *  Sign in the user upon button click.
  */
-function handleAuthClick() {
+function handleAuthClick1() {
   tokenClient.callback = async (resp) => {
     if (resp.error !== undefined) {
       throw resp;
     }
-    document.getElementById("signout_button").style.visibility = "visible";
-    document.getElementById("send_mail_button").style.visibility = "visible";
-    document.getElementById("authorize_button").innerText = "Refresh";
-    await listEmailIds();
+    $("#authorize-button").remove();
+    $(".table-inbox").removeClass("hidden");
+    $("#compose-button").removeClass("hidden");
+    $("#authorize_button").addClass("hidden");
+    displayInbox();
   };
 
   if (gapi.client.getToken() === null) {
@@ -108,81 +102,43 @@ function handleSignoutClick() {
   }
 }
 
-// Handle emails
-
-async function listEmailIds() {
-  let response;
-  try {
-     response = await gapi.client.gmail.users.messages.list({
-      userId: "me",
-      labelIds: "INBOX",
-    });
-
-    response.execute(function (response) {
-      $.each(response.messages, function () {
-        var messageRequest = gapi.client.gmail.users.messages.get({
-          userId: "me",
-          id: this.id,
-        });
-
-        messageRequest.execute(appendMessageRow);
-      });
-    });
-  } catch (err) {
-    document.getElementById("content").innerText = err.message;
-    return [];
-  }
-}
-
-async function readEmail(id) {
-  try {
-    response = await gapi.client.gmail.users.messages.get({
-      userId: "me",
-      id: id,
-      format: "raw",
-    });
-    let raw = response.result.raw;
-
-    var html = atob(raw.replace(/-/g, "+").replace(/_/g, "/"));
-    let popup = document.getElementById("popup-form");
-    let input = popup.getElementsByTagName("input");
-    let content = document.getElementById("content");
-    content.innerHTML = html;
-    popup.style.display = "flex";
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-function getHeader(headers, index) {
-  var header = "";
-  $.each(headers, function () {
-    if (this.name.toLowerCase() === index.toLowerCase()) {
-      header = this.value;
-    }
+function displayInbox() {
+  var request = gapi.client.gmail.users.messages.list({
+    userId: "me",
+    labelIds: "INBOX",
+    maxResults: 15,
   });
-  return header;
+  request.execute(function (response) {
+    $.each(response.messages, function () {
+      var messageRequest = gapi.client.gmail.users.messages.get({
+        userId: "me",
+        id: this.id,
+      });
+      messageRequest.execute(appendMessageRow);
+    });
+  });
 }
 
 function appendMessageRow(message) {
+  console.log(message.payload.headers);
   $(".table-inbox tbody").append(
     "<tr>\
-            <td>" +
+        <td>" +
       getHeader(message.payload.headers, "From") +
       '</td>\
-            <td>\
-              <a href="#message-modal-' +
+        <td>\
+          <a href="#message-modal-' +
       message.id +
       '" data-toggle="modal" id="message-link-' +
       message.id +
       '">' +
       getHeader(message.payload.headers, "Subject") +
       "</a>\
-            </td>\
-            <td>" +
+        </td>\
+        <td>" +
       getHeader(message.payload.headers, "Date") +
       "</td>\
-          </tr>"
+      </tr>"
   );
   var reply_to = (
     getHeader(message.payload.headers, "Reply-to") !== ""
@@ -197,48 +153,135 @@ function appendMessageRow(message) {
     '<div class="modal fade" id="message-modal-' +
       message.id +
       '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
-            <div class="modal-dialog modal-lg">\
-              <div class="modal-content">\
-                <div class="modal-header">\
-                  <button type="button"\
-                          class="close"\
-                          data-dismiss="modal"\
-                          aria-label="Close">\
-                    <span aria-hidden="true">&times;</span></button>\
-                  <h4 class="modal-title" id="myModalLabel">' +
+        <div class="modal-dialog modal-lg">\
+          <div class="modal-content">\
+            <div class="modal-header">\
+              <button type="button"\
+                      class="close"\
+                      data-dismiss="modal"\
+                      aria-label="Close">\
+                <span aria-hidden="true">&times;</span></button>\
+              <h4 class="modal-title" id="myModalLabel">' +
       getHeader(message.payload.headers, "Subject") +
       '</h4>\
-                </div>\
-                <div class="modal-body">\
-                  <iframe id="message-iframe-' +
+            </div>\
+            <div class="modal-body">\
+              <iframe id="message-iframe-' +
       message.id +
       '" srcdoc="<p>Loading...</p>">\
-                  </iframe>\
-                </div>\
-                <div class="modal-footer">\
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-                  <button type="button" class="btn btn-primary reply-button" data-dismiss="modal" data-toggle="modal" data-target="#reply-modal"\
-                  onclick="fillInReply(\
-                    \'' +
+              </iframe>\
+            </div>\
+            <div class="modal-footer">\
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+              <button type="button" class="btn btn-primary reply-button" data-dismiss="modal" data-toggle="modal" data-target="#reply-modal"\
+              onclick="fillInReply(\
+                \'' +
       reply_to +
       "', \
-                    '" +
+                '" +
       reply_subject +
       "', \
-                    '" +
+                '" +
       getHeader(message.payload.headers, "Message-ID") +
       "'\
-                    );\"\
-                  >Reply</button>\
-                </div>\
-              </div>\
+                );\"\
+              >Reply</button>\
             </div>\
-          </div>"
+          </div>\
+        </div>\
+      </div>"
   );
   $("#message-link-" + message.id).on("click", function () {
     var ifrm = $("#message-iframe-" + message.id)[0].contentWindow.document;
     $("body", ifrm).html(getBody(message.payload));
   });
+}
+
+function sendEmail() {
+  $("#send-button").addClass("disabled");
+
+  sendMessage(
+    {
+      To: $("#compose-to").val(),
+      Subject: $("#compose-subject").val(),
+    },
+    $("#compose-message").val(),
+    composeTidy()
+  );
+
+  return false;
+}
+
+function composeTidy() {
+  $("#compose-modal").modal("hide");
+  $("#compose-to").val("");
+  $("#compose-subject").val("");
+  $("#compose-message").val("");
+  $("#customFile").val();
+  $("#send-button").removeClass("disabled");
+}
+
+function sendReply() {
+  $("#reply-button").addClass("disabled");
+
+  sendMessage(
+    {
+      To: $("#reply-to").val(),
+      Subject: $("#reply-subject").val(),
+      "In-Reply-To": $("#reply-message-id").val(),
+    },
+    $("#reply-message").val(),
+    replyTidy()
+  );
+
+  return false;
+}
+
+function replyTidy() {
+  $("#reply-modal").modal("hide");
+  $("#reply-message").val("");
+  console.log($("#customFile"));
+  $("#customFile").val();
+  console.log($("#customFile"));
+  $("#reply-button").removeClass("disabled");
+}
+
+function fillInReply(to, subject, message_id) {
+  $("#reply-to").val(to);
+  $("#reply-subject").val(subject);
+  $("#reply-message-id").val(message_id);
+}
+
+function sendMessage(headers_obj, message, callback) {
+  // var email = "";
+
+  // for (var header in headers_obj)
+  //   email += header += ": " + headers_obj[header] + "\r\n";
+
+  // email += "\r\n" + message;
+
+  var email = createMimeMessage_(message)
+  
+  console.log(email);
+  var sendRequest = gapi.client.gmail.users.messages.send({
+    userId: "me",
+    resource: {
+      // raw: window.btoa(email).replace(/\+/g, "-").replace(/\//g, "_"),
+      raw: btoa(unescape(encodeURIComponent(email))),
+    },
+  });
+
+  return sendRequest.execute(callback);
+}
+
+function getHeader(headers, index) {
+  var header = "";
+  $.each(headers, function () {
+    if (this.name.toLowerCase() === index.toLowerCase()) {
+      header = this.value;
+    }
+  });
+  return header;
 }
 
 function getBody(message) {
@@ -255,93 +298,125 @@ function getBody(message) {
   return decodeURIComponent(escape(window.atob(encodedBody)));
 }
 
-async function getEmail(id) {
-  let response;
-  try {
-    response = await gapi.client.gmail.users.messages.get({
-      userId: "me",
-      id: id,
-      format: "full",
-    });
-
-    let item = response.result.payload.headers;
-
-    // Truy cập vào phần tử tbody của bảng
-    const tbody = document.querySelector("#data-table tbody");
-
-    const row = document.createElement("tr");
-
-    // Tạo các ô dữ liệu
-    const orderCell = document.createElement("td");
-    orderCell.textContent = i;
-    i++;
-
-    const nameCell = document.createElement("td");
-    const senderCell = document.createElement("td");
-    const senderEmailCell = document.createElement("td");
-    const timeCell = document.createElement("td");
-    const detail = document.createElement("td");
-
-    item.map((e) => {
-      if (e.name == "Subject") nameCell.textContent = e.value;
-      if (e.name == "From") {
-        const parts = e.value.split("<");
-        senderCell.textContent = parts[0].trim();
-        senderEmailCell.textContent = parts[1].replace(">", "").trim();
+function getHTMLPart(arr) {
+  for (var x = 0; x <= arr.length; x++) {
+    if (typeof arr[x].parts === "undefined") {
+      if (arr[x].mimeType === "text/html") {
+        return arr[x].body.data;
       }
-      if (e.name == "Date") timeCell.textContent = e.value;
-    });
-    const button = document.createElement("button");
-    button.textContent = "Detail";
-    button.onclick = () => readEmail(id);
-    detail.appendChild(button);
-
-    // Thêm các ô vào hàng
-    row.appendChild(orderCell);
-    row.appendChild(nameCell);
-    row.appendChild(senderCell);
-    row.appendChild(senderEmailCell);
-    row.appendChild(timeCell);
-    row.appendChild(detail);
-
-    // Thêm hàng vào tbody
-    tbody.appendChild(row);
-  } catch (err) {
-    document.getElementById("content").innerText = err.message;
-    return [];
+    } else {
+      return getHTMLPart(arr[x].parts);
+    }
   }
+  return "";
 }
 
-function openSendEmail() {
-  let popup = document.getElementById("popup-form-2");
-  popup.style.display = "flex";
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+function sendEmailWithAttachments() {
+  var attachments = ['File_ID_1', 'File_ID_2'];
+
+  var message = {
+    to: {
+      name: 'Google Scripts',
+      email: 'amit@labnol.org',
+    },
+    from: {
+      name: 'Amit Agarwal',
+      email: 'amit@labnol.org',
+    },
+    body: {
+      text: "Mr hänn is schon lang nümme g'she.",
+      html: "Mr hänn is schon <b>lang nümme</b> g'she.",
+    },
+    subject: 'ctrlq, tech à la carte',
+    files: getAttachments_(attachments),
+  };
+
+  // Compose Gmail message and send immediately
+  callGmailAPI_(message);
 }
 
-function sendEmail() {
-  const sender = "the.shy.garena2@gmail.com";
-  const receiver = document.getElementById("email_receiver");
-  const content = document.getElementById("content");
-  const subject = document.getElementById("tieu_de");
+function callGmailAPI_(message) {
+  var payload = createMimeMessage_(message);
 
-  const message = `From: ${sender}\r\n" + 
-"To: ${receiver}\r\n" +
-"Subject: ${subject}\r\n\r\n" +
- ${content} `;
-
-  // The body needs to be base64url encoded.
-  const encodedMessage = btoa(message);
-  const reallyEncodedMessage = encodedMessage
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-
-  gapi.client.gmail.users.messages
-    .send({
-      userId: "me",
-      resouce: {
-        raw: reallyEncodedMessage,
+  var response = UrlFetchApp.fetch(
+    'https://www.googleapis.com/upload/gmail/v1/users/me/messages/send?uploadType=media',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + ScriptApp.getOAuthToken(),
+        'Content-Type': 'message/rfc822',
       },
-    })
-    .then(function () {
-      console.log("done!");
+      muteHttpExceptions: true,
+      payload: payload,
+    }
+  );
+
+}
+
+// UTF-8 characters in names and subject
+function encode_(subject) {
+  var enc_subject = Utilities.base64Encode(subject, Utilities.Charset.UTF_8);
+  return '=?utf-8?B?' + enc_subject + '?=';
+}
+
+// Insert file attachments from Google Drive
+function getAttachments_(ids) {
+  var att = [];
+  for (var i in ids) {
+    var file = DriveApp.getFileById(ids[i]);
+    att.push({
+      mimeType: file.getMimeType(),
+      fileName: file.getName(),
+      bytes: Utilities.base64Encode(file.getBlob().getBytes()),
     });
+  }
+  return att;
+}
+
+// Create a MIME message that complies with RFC 2822
+function createMimeMessage_(msg) {
+  var nl = '\n';
+  var boundary = '__ctrlq_dot_org__';
+
+  var mimeBody = [
+    'MIME-Version: 1.0',
+    'To: ' + encode_(msg.to) ,
+    'From: ' + encode_(msg.from),
+    'Subject: ' + encode_(msg.subject), // takes care of accented characters
+
+    'Content-Type: multipart/alternative; boundary=' + boundary + nl,
+    '--' + boundary,
+
+    'Content-Type: text/plain; charset=UTF-8',
+    'Content-Transfer-Encoding: base64' + nl,
+    Utilities.base64Encode(msg.body.text, Utilities.Charset.UTF_8) + nl,
+    '--' + boundary,
+
+    'Content-Type: text/html; charset=UTF-8',
+    'Content-Transfer-Encoding: base64' + nl,
+    Utilities.base64Encode(msg.body.html, Utilities.Charset.UTF_8) + nl,
+  ];
+
+  for (var i = 0; i < msg.files.length; i++) {
+    var attachment = [
+      '--' + boundary,
+      'Content-Type: ' + msg.files[i].mimeType + '; name="' + msg.files[i].fileName + '"',
+      'Content-Disposition: attachment; filename="' + msg.files[i].fileName + '"',
+      'Content-Transfer-Encoding: base64' + nl,
+      msg.files[i].bytes,
+    ];
+
+    mimeBody.push(attachment.join(nl));
+  }
+
+  mimeBody.push('--' + boundary + '--');
+
+  return mimeBody.join(nl);
 }
